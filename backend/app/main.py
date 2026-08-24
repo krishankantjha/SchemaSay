3498656294107baafcc13eaa3b7e0c1ecfd46d3c
Krishan Kant Jha <jhakrishankant89@gmail.com>
2026-08-24 18:42:56 +0000

    Allow connection updates from the browser

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,8 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "Accept"],
 )
 
 
